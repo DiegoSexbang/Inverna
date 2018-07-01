d29ac5e610fb52c86fa351799e62399281c25db3
DiegoSexbang <diegoxd96@gmail.com>
2018-06-30 20:46:56 -0400

    filtro

--- Inverna/Controllers/LecturaController.cs
+++ Inverna/Controllers/LecturaController.cs
@@ -17,20 +17,44 @@ namespace Inverna.Controllers
     public class LecturaController : Controller
     {
         private BD db = new BD();
-        
+
 
 
         public ActionResult Index()
         {
             Lectura let = new Lectura();
             List<Lectura> lista = let.lect();
-            
-            return View(lista);
+
+
+            ViewBag.TotalLectura = lista;
+            return View();
         }
 
+        [HttpPost]
+        public JsonResult Filtrar(int id)
+        {
+            var listalectura = db.Lectura.ToList();
+            if(id != 0 )
+                listalectura = listalectura.Where(l => l.tipolectura_idtipolectura == id).ToList();
+            List<Pico> lista = new List<Pico>();
+            foreach(var lectura in listalectura)
+            {
+                Pico pico = new Pico();
+                pico.Valor = lectura.valor;
+                pico.Fecha = lectura.hora.ToString();
+                pico.TipoLectura = lectura.TipoLectura.nombre;
+                lista.Add(pico);
+            }
+            //var lista = JsonConvert.SerializeObject(listalectura, Formatting.None, new JsonSerializerSettings() { ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore });
+            return Json(lista);
+        }
 
-
-        
+        public class Pico
+        {
+            public double Valor { get; set; }
+            public string Fecha { get; set; }
+            public string TipoLectura { get; set; }
+        }
 
 
 
